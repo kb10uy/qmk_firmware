f4b4dc30812aa953433b6c718278de2637e5e70f
Yū Kobayashi <kb10uy@kb10uy.org>
2021-02-16 00:33:08 +0900

    Tweaks rates

--- keyboards/crkbd/rev1/common/keymaps/kb10uy/config.h
+++ keyboards/crkbd/rev1/common/keymaps/kb10uy/config.h
@@ -33,7 +33,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 #define USE_SERIAL_PD2
+#define LAYER_STATE_8BIT
 
+#undef DEBOUNCE
+#define DEBOUNCE 3
+#define USB_POLLING_INTERVAL_MS 2
+// #define DEBUG_MATRIX_SCAN_RATE
 
 #undef RGBLED_NUM
 // #define RGBLIGHT_ANIMATIONS
--- keyboards/crkbd/rev1/common/keymaps/kb10uy/rules.mk
+++ keyboards/crkbd/rev1/common/keymaps/kb10uy/rules.mk
@@ -3,3 +3,4 @@ RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
 OLED_DRIVER_ENABLE  = yes
 TAP_DANCE_ENABLE    = yes
 LTO_ENABLE          = yes
+CONSOLE_ENABLE      = no
