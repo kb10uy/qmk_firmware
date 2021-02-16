b42a96d324bff8749c6e9a712b5406e95303f647
Yū Kobayashi <kb10uy@kb10uy.org>
2021-02-16 17:11:08 +0900

    Copies transport.{ch} into kb10uy keymap

--- keyboards/crkbd/rev1/common/keymaps/kb10uy/rules.mk
+++ keyboards/crkbd/rev1/common/keymaps/kb10uy/rules.mk
@@ -4,3 +4,7 @@ OLED_DRIVER_ENABLE  = yes
 TAP_DANCE_ENABLE    = yes
 LTO_ENABLE          = yes
 CONSOLE_ENABLE      = no
+
+SPLIT_TRANSPORT     = custom
+QUANTUM_LIB_SRC    += serial.c
+SRC                += transport_ex.c
