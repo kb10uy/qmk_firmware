834556f7b605a8fe8a01b10c8f5c5a29453c1049
Yū Kobayashi <kb10uy@kb10uy.org>
2021-03-07 12:17:38 +0900

    Update transport_ex.{ch}

--- keyboards/crkbd/rev1/common/keymaps/kb10uy/transport_ex.h
+++ keyboards/crkbd/rev1/common/keymaps/kb10uy/transport_ex.h
@@ -6,7 +6,5 @@ void transport_master_init(void);
 void transport_slave_init(void);
 
 // returns false if valid data not received from slave
-uint8_t transport_get_sync(void);
-void transport_set_sync(uint8_t data);
-bool transport_master(matrix_row_t matrix[]);
-void transport_slave(matrix_row_t matrix[]);
+bool transport_master(matrix_row_t master_matrix[], matrix_row_t slave_matrix[]);
+void transport_slave(matrix_row_t master_matrix[], matrix_row_t slave_matrix[]);
